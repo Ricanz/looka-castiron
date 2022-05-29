63cc921fd9536f4e15d9c0f39af3cf6f57baf26e
erza <58721335+erzaffasya@users.noreply.github.com>
2022-05-29 18:15:50 +0800

    factory

---
+++ database/factories/AboutUsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AboutUs>
+ */
+class AboutUsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'deskripsi' => $this->faker->name(),
+            'gambar'=>'https://source.unsplash.com/random',
+        ];
+    }
+}

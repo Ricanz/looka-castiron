e3b7a5dd4f4f36998ecf654d07ddfbfc6033bb3a
ctriyanti <riyanti@cashtree.id>
2022-08-03 18:35:59 +0700

    detail product

--- resources/views/guest/katalog/detail.blade.php
+++ resources/views/guest/katalog/detail.blade.php
@@ -55,4 +55,88 @@
     </section>
     <!-- Feature & Special Section End -->
     <!-- offer Section End -->
+
+    <section class="ec-banner section section-space-p">
+        <h2 class="d-none">Banner</h2>
+        <div class="container">
+            <div class="row m-tb-minus-15">
+                <div class="ec-banners">
+                    <div class="ec-banner-left col-sm-6">
+                        <div class="ec-banner-block ec-banner-block-1 col-sm-12">
+                            <div class="banner-block">
+                                <img src="{{ asset('tlandingPage/assets/images/looka/16.png') }}" alt="" />
+                                <div class="banner-content">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ec-banner-right col-sm-6">
+                        <div class="ec-banner-block ec-banner-block-2 col-sm-12">
+                            <div class="banner-block">
+                                <img src="{{ asset('tlandingPage/assets/images/looka/15.png') }}" alt="" />
+                                <div class="banner-content">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section ec-services-section">
+        <h2 class="d-none">Services</h2>
+        <div class="container">
+            <div class="row">
+                <div class="ec_ser_content ec_ser_content_1 col-sm-12 col-md-3">
+                    <div class="ec_ser_inner">
+                        <div class="ec-service-image">
+                            <img src="{{ asset('tlandingPage/assets/images/icons/service_5_1.svg') }}"
+                                class="svg_img" alt="" />
+                        </div>
+                        <div class="ec-service-desc">
+                            <h2>kualitas</h2>
+                            <p>memilih iron kualitas terbaik yang cocok untuk makanan</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="ec_ser_content ec_ser_content_2 col-sm-12 col-md-3">
+                    <div class="ec_ser_inner">
+                        <div class="ec-service-image">
+                            <img src="{{ asset('tlandingPage/assets/images/icons/service_2.svg') }}"
+                                class="svg_img" alt="" />
+                        </div>
+                        <div class="ec-service-desc">
+                            <h2>kontak kami</h2>
+                            <p>seller dapat dihubungi anytime</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="ec_ser_content ec_ser_content_3 col-sm-12 col-md-3">
+                    <div class="ec_ser_inner">
+                        <div class="ec-service-image">
+                            <img src="{{ asset('tlandingPage/assets/images/icons/service_3.svg') }}"
+                                class="svg_img" alt="" />
+                        </div>
+                        <div class="ec-service-desc">
+                            <h2>online shopping</h2>
+                            <p>temukan kami di online shop kesayangan kalian</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="ec_ser_content ec_ser_content_4 col-sm-12 col-md-3">
+                    <div class="ec_ser_inner">
+                        <div class="ec-service-image">
+                            <img src="{{ asset('tlandingPage/assets/images/icons/service_5_4.svg') }}"
+                                class="svg_img" alt="" />
+                        </div>
+                        <div class="ec-service-desc">
+                            <h2>harga terjangkau</h2>
+                            <p>harga dan kualitas sejajar</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-guest-layout>

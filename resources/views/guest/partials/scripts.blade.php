78285c21d6ca45b6488a0904fe64f31912183efe
ctriyanti <riyanti@cashtree.id>
2022-08-08 13:18:03 +0700

    floating button

--- resources/views/guest/partials/scripts.blade.php
+++ resources/views/guest/partials/scripts.blade.php
@@ -1,3 +1,86 @@
+<div class="ec-cp-style cp-right-bottom">
+    <!-- Start Floating Panel Container -->
+    <div class="cp-panel">	
+        <!-- Chat Button For Facebook Messenger -->
+        <!-- Chat Button For Whatsapp -->
+        <a id="cp-whatsapp" href="#" class="cp-list" data-number="919099153528" data-message="Please help me! I have got wrong product from your website." target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/chat-pro/cp-whatsapp.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Whatsapp</h2>
+                <p>directlly chat on Whatsapp</p>
+            </div>
+        </a>
+        <!-- Chat Button For Instagram -->
+        <a class="cp-list" href="https://www.instagram.com/_k_e_t_a_n_/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/chat-pro/cp-instagram.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Instagram</h2>
+                <p>discuss over Instagram</p>
+            </div>
+        </a>
+        <!-- Chat Button For LinkedIn -->
+        <a class="cp-list" href="https://www.linkedin.com/messaging/thread/6599949383096205313/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/chat-pro/cp-linkedin.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>LinkedIn</h2>
+                <p>discuss over LinkedIn</p>
+            </div>
+        </a>
+        <!-- Chat Button For Twitter -->
+        <a class="cp-list" href="https://twitter.com/messages/1899733261-2826370326/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/chat-pro/cp-twitter.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Twitter</h2>
+                <p>easily chat over Twitter</p>
+            </div>
+        </a>
+        <!-- Chat Button For Shopee -->
+        <a class="cp-list" href="https://twitter.com/messages/1899733261-2826370326/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/shopee-s.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Shopee</h2>
+                <p>Visit us on Shopee</p>
+            </div>
+        </a>
+        <!-- Chat Button For Tokopedia -->
+        <a class="cp-list" href="https://twitter.com/messages/1899733261-2826370326/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/tokopedia-s.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Tokopedia</h2>
+                <p>Visit us on Tokopedia</p>
+            </div>
+        </a>
+
+        <!-- Chat Button For Lazada -->
+        <a class="cp-list" href="https://twitter.com/messages/1899733261-2826370326/" target="_blank">
+            <div class="cp-image">
+                <img src="{{('tlandingPage/assets/images/lazaa-s.png')}}" alt=""/>
+            </div>	
+            <div class="cp-content">
+                <h2>Lazada</h2>
+                <p>Visit us on Lazada</p>
+            </div>
+        </a>
+    </div>
+    <!--/ End Floating Panel Container -->
+    <!-- Start Right Floating Button -->
+    <div class="cp-button cp-right-bottom">
+        <img src="{{('tlandingPage/assets/images/chat-pro/chat.png')}}" alt="icon"/>
+    </div>
+    <!--/ End Right Floating Button -->
+</div>
     <!-- Vendor JS -->
     <script src="{{asset('tlandingPage/assets/js/vendor/jquery-3.5.1.min.js')}}"></script>
     <script src="{{asset('tlandingPage/assets/js/vendor/popper.min.js')}}"></script>

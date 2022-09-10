7a86351c54654c0f251f8fda3a06e33e59e69b40
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-09-10 16:51:35 +0700

    update detail meta

--- resources/views/guest/katalog/detail.blade.php
+++ resources/views/guest/katalog/detail.blade.php
@@ -13,10 +13,10 @@
      <meta http-equiv="x-ua-compatible" content="ie=edge" />
      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
      
-     <title>Ekka - Ecommerce HTML Template.</title>
-     <meta name="keywords" content="apparel, catalog, clean, ecommerce, ecommerce HTML, electronics, fashion, html eCommerce, html store, minimal, multipurpose, multipurpose ecommerce, online store, responsive ecommerce template, shops" />
-     <meta name="description" content="Best ecommerce html template for single and multi vendor store.">
-     <meta name="author" content="ashishmaraviya">
+    <title>Looka Cast Iron</title>
+    <meta name="keywords" content="hotplate, shopping, food" />
+    <meta name="description" content="Looka Castiron adalah toko penyedia hotplate dengan kualitas terbaik dan harga terjangkau">
+    <meta name="author" content="looka">
      
     <!-- site Favicon -->
     <link rel="icon" href="{{asset('tlandingPage/assets/images/favicon/favicon.png')}}" sizes="32x32" />

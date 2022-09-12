9becfbe42b9e8930658a774e5a9a094eba571ee3
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-09-12 22:29:31 +0700

    add short_desc

--- resources/views/guest/katalog/kategori.blade.php
+++ resources/views/guest/katalog/kategori.blade.php
@@ -121,7 +121,7 @@
                                         </div>
                                     </div>
                                     <div class="pro-hidden-block">
-                                        <div class="ec-pro-desc" align='left'>{!! Str::limit($item->deskripsi, 150) !!}</div>
+                                        <div class="ec-pro-desc" align='left'>{!! Str::limit($item->short_desc, 150) !!}</div>
                                     </div>
                                 </div>
                             </div>

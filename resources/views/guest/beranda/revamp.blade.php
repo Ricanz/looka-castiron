078bdf4648f9efbe04ad2790f77cb6ea8524d97b
ctriyanti <riyanti@cashtree.id>
2023-03-28 08:54:02 +0700

    tentang kami

--- resources/views/guest/beranda/revamp.blade.php
+++ resources/views/guest/beranda/revamp.blade.php
@@ -13,8 +13,7 @@
     <!-- Main Slider End -->
     
     <!--  Feature & Special Section Start -->
-    <section class="section ec-exe-spe-section section-space-ptb-100 section-space-mt section-space-mb-100"
-        style="background-image: url('{{asset('tlandingPage/assets/images/special-product/background.jpg')}}');">
+    <section class="section ec-exe-spe-section section-space-ptb-100 section-space-mt section-space-mb-100">
         <div class="container">
             <div class="row">
                 <!--  Special Section Start -->
@@ -28,7 +27,7 @@
                     <div class="ec-spe-products">
                         <div class="ec-fs-product">
                             <div class="ec-fs-pro-inner ec-product-inner">
-                                <div class="ec-fs-pro-image-outer col-lg-6 col-md-6 col-sm-6">
+                                <div class="ec-fs-pro-image-outer col-lg-4 col-md-6 col-sm-6">
                                     <div class="ec-fs-pro-image about-image">
                                         @if ($tentang != null)
                                         <a href="#" class="image"><img class="main-image"
@@ -36,9 +35,9 @@
                                         @endif                                       
                                     </div>
                                 </div>
-                                <div class="ec-pro-content col-lg-6 col-md-6 col-sm-6">
+                                <div class="ec-pro-content col-lg-8 col-md-6 col-sm-6">
                                     <div class="countdowntimer">
-                                        <span class="ec-fs-count-desc" align="justify">
+                                        <span class="ec-fs-count-desc about-content" align="justify">
                                             @if ($tentang != null)
                                             {!! $tentang->deskripsi !!}
                                             @endif

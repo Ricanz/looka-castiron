c092e2cae265ec7274498fcf103e7d42cd498e79
ctriyanti <riyanti@cashtree.id>
2023-03-31 23:26:40 +0700

    3000

--- resources/views/guest/beranda/revamp.blade.php
+++ resources/views/guest/beranda/revamp.blade.php
@@ -317,7 +317,7 @@
                 slideIndex++;
                 if (slideIndex > x.length) {slideIndex = 1}
                 x[slideIndex-1].style.display = "block";
-                setTimeout(carousel, 2000); // Change image every 2 seconds
+                setTimeout(carousel, 3000); // Change image every 2 seconds
             }
         });
     </script>
